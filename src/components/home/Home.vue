f68c290d3d3f09ba5a3be3a8a2dc1b7b44156514
Shoudong <2407592711@qq.com>
2020-04-03 12:28:37 +0800

    修改

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -5,6 +5,7 @@
         <div class="mui-slider-item mui-slider-loop">12</div>
         <div class="mui-slider-item">13</div>
         <div class="mui-slider-item">14</div>
+        
       </div>
     </div>
   </div>
